9172e8a5880fe30133d63f2d877416243add30f5
Mario Bălănică <mariobalanica02@gmail.com>
2023-04-05 01:35:10 +0300

    Silicon/Rockchip: Add RK3588 OTP library

---
+++ edk2-platforms/Silicon/Rockchip/Include/Library/OtpLib.h
@@ -0,0 +1,35 @@
+/** @file
+ *
+ *  Copyright (c) 2022, Jared McNeill <jmcneill@invisible.ca>
+ *  Copyright (c) 2023, Mario Bălănică <mariobalanica02@gmail.com>
+ *
+ *  SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ **/
+
+#ifndef OTPLIB_H__
+#define OTPLIB_H__
+
+VOID
+OtpRead (
+    IN UINT16 Offset,
+    IN UINT16 Length,
+    OUT UINT8 *Data
+    );
+
+VOID
+OtpReadCpuCode (
+    OUT UINT16 *CpuCode
+    );
+
+VOID
+OtpReadId (
+    OUT UINT8 Id[16]
+    );
+
+VOID
+OtpReadCpuVersion (
+    OUT UINT8 *Version
+    );
+
+#endif /* OTPLIB_H__ */
