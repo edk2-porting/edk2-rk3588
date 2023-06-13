40db0be757657e86a72a6e3c503963ae49e9da7b
Mario Bălănică <mariobalanica02@gmail.com>
2023-06-13 05:03:30 +0300

    Silicon/Rockchip: Add PlatformBootDescriptionLib
    This filters the boot descriptions in UefiBootManagerLib and returns proper ones for the SD & eMMC Block I/O devices.
    
    Additionally, it appends "(Not Connected)" to devices without a media present and "[Firmware]" to the boot device that holds the UEFI image.

---
+++ edk2-rockchip/Silicon/Rockchip/Library/PlatformBootDescriptionLib/PlatformBootDescriptionLib.c
@@ -0,0 +1,97 @@
+/** @file
+ *
+ *  Boot Manager device description handler.
+ *
+ *  Copyright (c) 2023, Mario Bălănică <mariobalanica02@gmail.com>
+ *
+ *  SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ **/
+
+#include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiBootManagerLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/RkAtagsLib.h>
+#include <Protocol/BlockIo.h>
+
+RKATAG_BOOTDEV_TYPE  mBootDeviceType;
+
+CHAR16               mBootDescNotConnectedSuffix[]  = L" (Not Connected)";
+CHAR16               mBootDescFirmwareSuffix[]      = L" [Firmware]";
+
+STATIC
+CHAR16*
+EFIAPI
+PlatformBootDescriptionHandler (
+  IN EFI_HANDLE    Handle,
+  IN CONST CHAR16  *DefaultDescription
+  )
+{
+  EFI_STATUS              Status;
+  EFI_DEV_PATH            *Device;
+  CHAR16                  *Description;
+  UINTN                   MaxDescriptionSize;
+  BOOLEAN                 IsBootDevice;
+  EFI_BLOCK_IO_PROTOCOL   *BlkIo;
+  BOOLEAN                 IsMediaPresent;
+
+  Device = (EFI_DEV_PATH *) DevicePathFromHandle (Handle);
+
+  if (Device->DevPath.Type != HARDWARE_DEVICE_PATH || Device->DevPath.SubType != HW_MEMMAP_DP) {
+    return NULL;
+  }
+
+  Description = NULL;
+  IsBootDevice = FALSE;
+
+  if (Device->MemMap.StartingAddress == PcdGet32 (PcdSdhciDxeBaseAddress)) {
+    Description = L"UEFI eMMC Device";
+    IsBootDevice = mBootDeviceType == RkAtagBootDevTypeEmmc;
+  } else if (Device->MemMap.StartingAddress == PcdGet32 (PcdDwEmmcDxeBaseAddress)) {
+    Description = L"UEFI SD Device";
+    IsBootDevice = mBootDeviceType == RkAtagBootDevTypeSd0;
+  } else {
+    return NULL;
+  }
+
+  MaxDescriptionSize = StrSize (Description) - sizeof (CHAR16)
+                       + StrSize (mBootDescNotConnectedSuffix) - sizeof (CHAR16)
+                       + StrSize (mBootDescFirmwareSuffix);
+
+  IsMediaPresent = FALSE;
+  Status = gBS->HandleProtocol (Handle, &gEfiBlockIoProtocolGuid, (VOID *) &BlkIo);
+  if (!EFI_ERROR (Status)) {
+    IsMediaPresent = BlkIo->Media->MediaPresent;
+  }
+
+  Description = AllocateCopyPool (MaxDescriptionSize, Description);
+  if (Description == NULL) {
+    return NULL;
+  }
+
+  if (!IsMediaPresent) {
+    StrCatS (Description, MaxDescriptionSize / sizeof (CHAR16), mBootDescNotConnectedSuffix);
+  }
+  if (IsBootDevice) {
+    StrCatS (Description, MaxDescriptionSize / sizeof (CHAR16), mBootDescFirmwareSuffix);
+  }
+
+  return Description;
+}
+
+EFI_STATUS
+EFIAPI
+PlatformBootDescriptionLibConstructor (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  RKATAG_BOOTDEV *BootDevice;
+
+  BootDevice = RkAtagsGetBootDev ();
+  mBootDeviceType = BootDevice != NULL ? BootDevice->DevType : RkAtagBootDevTypeUnknown;
+
+  return EfiBootManagerRegisterBootDescriptionHandler (PlatformBootDescriptionHandler);
+}
