9c10dafb3708e1b7df132900567261b8088686f3
Mario Bălănică <mariobalanica02@gmail.com>
2023-06-24 01:05:24 +0300

    Silicon/RK3588: Expose PCIe & SATA in ACPI depending on configuration
    This implementation based on RPi's ConfigDxe is good enough for now.
    Might come back to it later and rework using dynamic tables with generators, so that PCIe resources, MCFG, MADT and IORT can be entirely populated by code.

--- edk2-rockchip/Silicon/Rockchip/RK3588/Drivers/RK3588Dxe/RK3588Dxe.c
+++ edk2-rockchip/Silicon/Rockchip/RK3588/Drivers/RK3588Dxe/RK3588Dxe.c
@@ -13,6 +13,7 @@
 #include <Library/DebugLib.h>
 #include <Library/DevicePathLib.h>
 #include <Library/IoLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/PcdLib.h>
 #include <Library/UefiBootServicesTableLib.h>
@@ -109,6 +110,163 @@ InstallSataDevices (
   }
 }
 
+STATIC CONST EFI_GUID mAcpiTableFile = {
+  0x7E374E25, 0x8E01, 0x4FEE, { 0x87, 0xf2, 0x39, 0x0C, 0x23, 0xC6, 0x06, 0xCD }
+};
+
+typedef struct {
+  CHAR8 Name[4];
+  UINTN PcdToken;
+} AML_NAME_OP_REPLACE;
+
+#define SSDT_PATTERN_LEN 5
+#define AML_NAMEOP_8   0x0A
+#define AML_NAMEOP_16  0x0B
+#define AML_NAMEOP_32  0x0C
+#define AML_NAMEOP_STR 0x0D
+//
+// Scan the given namespace table (DSDT/SSDT) for AML NameOps
+// listed in the NameOpReplace structure. If one is found then
+// update the value in the table from the specified Pcd
+//
+// This allows us to have conditionals in AML controlled
+// by options in the BDS or detected during firmware bootstrap.
+// We could extend this concept for strings/etc but due to len
+// variations its probably easier to encode the strings
+// in the ASL and pick the correct one based off a variable.
+//
+STATIC
+VOID
+AcpiUpdateSdtNameOps (
+  EFI_ACPI_DESCRIPTION_HEADER  *AcpiTable,
+  CONST AML_NAME_OP_REPLACE    *NameOpReplace
+  )
+{
+  UINTN  Idx;
+  UINTN  Index;
+  CHAR8  Pattern[SSDT_PATTERN_LEN];
+  UINTN  PcdVal;
+  UINT8  *SdtPtr;
+  UINT32 SdtSize;
+
+  SdtSize = AcpiTable->Length;
+
+  if (SdtSize > 0) {
+    SdtPtr = (UINT8 *)AcpiTable;
+
+    for (Idx = 0; NameOpReplace && NameOpReplace[Idx].PcdToken; Idx++) {
+      //
+      // Do a single NameOp variable replacement these are of the
+      // form 08 XXXX SIZE VAL, where SIZE is 0A=byte, 0B=word, 0C=dword
+      // and XXXX is the name and VAL is the value
+      //
+      Pattern[0] = 0x08;
+      Pattern[1] = NameOpReplace[Idx].Name[0];
+      Pattern[2] = NameOpReplace[Idx].Name[1];
+      Pattern[3] = NameOpReplace[Idx].Name[2];
+      Pattern[4] = NameOpReplace[Idx].Name[3];
+
+      for (Index = 0; Index < (SdtSize - SSDT_PATTERN_LEN); Index++) {
+        if (CompareMem (SdtPtr + Index, Pattern, SSDT_PATTERN_LEN) == 0) {
+          PcdVal = LibPcdGet32 (NameOpReplace[Idx].PcdToken);
+          switch (SdtPtr[Index + SSDT_PATTERN_LEN]) {
+          case AML_NAMEOP_32:
+            SdtPtr[Index + SSDT_PATTERN_LEN + 4] = (PcdVal >> 24) & 0xFF;
+            SdtPtr[Index + SSDT_PATTERN_LEN + 3] = (PcdVal >> 16) & 0xFF;
+            // Fallthrough
+          case AML_NAMEOP_16:
+            SdtPtr[Index + SSDT_PATTERN_LEN + 2] = (PcdVal >> 8) & 0xFF;
+            // Fallthrough
+          case AML_NAMEOP_8:
+            SdtPtr[Index + SSDT_PATTERN_LEN + 1] = PcdVal & 0xFF;
+            break;
+          case 0:
+          case 1:
+            SdtPtr[Index + SSDT_PATTERN_LEN + 1] = !!PcdVal;
+            break;
+          case AML_NAMEOP_STR:
+            //
+            // If the string val is added to the NameOpReplace, we can
+            // dynamically update things like _HID too as long as the
+            // string length matches.
+            //
+            break;
+          }
+          break;
+        }
+      }
+    }
+  }
+}
+
+STATIC
+BOOLEAN
+AcpiVerifyUpdateTable (
+  IN  EFI_ACPI_DESCRIPTION_HEADER   *AcpiHeader
+  )
+{
+  BOOLEAN              Result;
+  AML_NAME_OP_REPLACE  *NameOpReplace;
+
+  Result = TRUE;
+  NameOpReplace = NULL;
+
+  switch (AcpiHeader->OemTableId) {
+    case SIGNATURE_64 ('P', 'C', 'I', 'E', '3', '4', 'L', '0'):
+      Result = FixedPcdGetBool (PcdPcie30Supported)
+                && PcdGet32 (PcdPcie30State) == PCIE30_STATE_ENABLED;
+      break;
+    case SIGNATURE_64 ('P', 'C', 'I', 'E', '3', '2', 'L', '0'):
+      Result = FALSE; // not supported yet
+      break;
+    case SIGNATURE_64 ('P', 'C', 'I', 'E', '2', '1', 'L', '0'):
+      Result = PcdGet32 (PcdComboPhy1Mode) == COMBO_PHY_MODE_PCIE;
+      break;
+    case SIGNATURE_64 ('P', 'C', 'I', 'E', '2', '1', 'L', '1'):
+      Result = PcdGet32 (PcdComboPhy2Mode) == COMBO_PHY_MODE_PCIE;
+      break;
+    case SIGNATURE_64 ('P', 'C', 'I', 'E', '2', '1', 'L', '2'):
+      Result = PcdGet32 (PcdComboPhy0Mode) == COMBO_PHY_MODE_PCIE;
+      break;
+    case SIGNATURE_64 ('A', 'H', 'C', 'S', 'A', 'T', 'A', '0'):
+      Result = PcdGet32 (PcdComboPhy0Mode) == COMBO_PHY_MODE_SATA;
+      break;
+    case SIGNATURE_64 ('A', 'H', 'C', 'S', 'A', 'T', 'A', '1'):
+      Result = PcdGet32 (PcdComboPhy1Mode) == COMBO_PHY_MODE_SATA;
+      break;
+    case SIGNATURE_64 ('A', 'H', 'C', 'S', 'A', 'T', 'A', '2'):
+      Result = PcdGet32 (PcdComboPhy2Mode) == COMBO_PHY_MODE_SATA;
+      break;
+  }
+
+  if (Result && NameOpReplace) {
+    AcpiUpdateSdtNameOps (AcpiHeader, NameOpReplace);
+  }
+
+  return Result;
+}
+
+//
+// Monitor the ACPI tables being installed and when
+// a DSDT/SSDT is detected validate that we want to
+// install it, and if so update any "NameOp" defined
+// variables contained in the table from PCD values
+//
+STATIC
+BOOLEAN
+AcpiHandleDynamicNamespace (
+  IN  EFI_ACPI_DESCRIPTION_HEADER   *AcpiHeader
+  )
+{
+  switch (AcpiHeader->Signature) {
+    case SIGNATURE_32 ('D', 'S', 'D', 'T'):
+    case SIGNATURE_32 ('S', 'S', 'D', 'T'):
+      return AcpiVerifyUpdateTable (AcpiHeader);
+  }
+
+  return TRUE;
+}
+
 STATIC
 EFI_STATUS
 EFIAPI
@@ -167,6 +325,9 @@ AfterApplyVariablesInit (
   )
 {
   InstallSataDevices ();
+
+  // TO-DO: Add a HII variable to let the user decide if they want ACPI (and/or DT).
+  LocateAndInstallAcpiFromFvConditional (&mAcpiTableFile, &AcpiHandleDynamicNamespace);
 }
 
 STATIC
@@ -267,10 +428,6 @@ RK3588InitPeripherals (
 
   return EFI_SUCCESS;
 }
-
-STATIC CONST EFI_GUID mAcpiTableFile = {
-  0x7E374E25, 0x8E01, 0x4FEE, { 0x87, 0xf2, 0x39, 0x0C, 0x23, 0xC6, 0x06, 0xCD }
-};
 
 STATIC VOID SetFlashAttributeToUncache(VOID)
 {
@@ -427,9 +584,6 @@ RK3588EntryPoint (
   }
 
   SetFlashAttributeToUncache();
-
-  // TO-DO: Add a HII variable to let the user decide if they want ACPI (and/or DT).
-  LocateAndInstallAcpiFromFvConditional (&mAcpiTableFile, NULL);
 
   return Status;
 }
